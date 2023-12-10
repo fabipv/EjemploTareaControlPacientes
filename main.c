e0f8af753999fa933fc8f74b411e38187ac512db
fabipv <112329085+fabipv@users.noreply.github.com>
2023-12-10 23:11:37 +0000

    Add struct and TDA list

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+#include "list.h"
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
+#define MAXCHAR 31
+
+typedef struct {
+  char nombre[MAXCHAR];
+  char apellido[MAXCHAR];
+  int edad;
+  char telefono[MAXCHAR];
+  char direccion[MAXCHAR];
+  char numSeguridad[MAXCHAR];
+  List *medicosAsignados;
+} Paciente;
+
+typedef struct {
+  char nombreMedico[MAXCHAR];
+} Medico;
