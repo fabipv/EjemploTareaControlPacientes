9056b62429579d80c20ff9214d3ba674691fb01d
fabipv <112329085+fabipv@users.noreply.github.com>
2023-12-10 23:28:39 +0000

    Add mostrarMenu() function

--- .ccls-cache/@home@runner@EjemploTareaControlPacientes/main.c
+++ .ccls-cache/@home@runner@EjemploTareaControlPacientes/main.c
@@ -20,3 +20,139 @@ typedef struct {
 typedef struct {
   char nombreMedico[MAXCHAR];
 } Medico;
+
+void mostrarMenu() {
+  puts("========================================");
+  puts("          Control de Pacientes");
+  puts("========================================");
+
+  puts("1) Registrar Paciente.");
+  puts("2) Importar Pacientes desde csv.");
+  puts("3) Exportar Pacientes a csv.");
+  puts("4) Salir.");
+}
+
+int main(int argc, char *argv[]) {
+
+  int opcion;
+  List *listaPacientes = createList();
+
+  do {
+    system("cls");
+    mostrarMenu();
+    puts("");
+    printf("Eliga una opcion: ");
+    scanf("%d", &opcion);
+    getchar();
+
+    switch (opcion) {
+    case 1:
+      system("cls");
+
+      char nombreAux[MAXCHAR];
+      char apellidoAux[MAXCHAR];
+      int edadAux;
+      char telefonoAux[MAXCHAR];
+      char direccionAux[MAXCHAR];
+      char numSeguridadAux[MAXCHAR];
+      List *medicosIngresados = createList();
+
+      puts("========================================");
+      puts("          Registrar Paciente:");
+      puts("========================================");
+
+      printf("Ingrese Nombre: ");
+      scanf("%30[^\n]", nombreAux);
+
+      while (getchar() != '\n')
+        ;
+
+      printf("Ingrese Apellido: ");
+      scanf("%30[^\n]s", apellidoAux);
+
+      while (getchar() != '\n')
+        ;
+
+      do {
+        printf("Ingrese Edad: ");
+
+        while (scanf("%d", &edadAux) != 1) {
+          printf("\nEntrada invalida. Ingrese un numero entero: ");
+          while (getchar() != '\n')
+            ;
+        }
+
+        if (edadAux <= 0) {
+          printf("\nEdad mal ingresada, intente nuevamente.\n");
+        }
+
+      } while (edadAux <= 0);
+
+      while (getchar() != '\n')
+        ;
+
+      printf("Ingrese Telefono: ");
+      scanf("%30[^\n]s", telefonoAux);
+
+      while (getchar() != '\n')
+        ;
+
+      printf("Ingrese Direccion (sin comas): ");
+      scanf("%30[^\n]s", direccionAux);
+
+      while (getchar() != '\n')
+        ;
+
+      printf("Ingrese Numero de Seguridad Social: ");
+      scanf("%30[^\n]s", numSeguridadAux);
+
+      while (getchar() != '\n')
+        ;
+
+      int respuestaMedicos;
+
+      do {
+        printf("\nDesea Asignar Medicos al Paciente\n");
+        printf("Si (1)\n");
+        printf("No (0)\n");
+
+        while (scanf("%d", &respuestaMedicos) != 1 ||
+               (respuestaMedicos != 1 && respuestaMedicos != 0)) {
+          printf("\nRespuesta invalida, intente nuevamente.\n");
+          printf("Si (1)\n");
+          printf("No (0)\n");
+          while (getchar() != '\n')
+            ;
+        }
+
+      } while (respuestaMedicos != 1 && respuestaMedicos != 0);
+
+      if (respuestaMedicos == 1) {
+        // registrarMedicos(medicosIngresados);
+      }
+
+      // registrarPaciente(listaPacientes, nombreAux, apellidoAux, edadAux,
+      // telefonoAux, direccionAux, numSeguridadAux, medicosIngresados, 1);
+      break;
+    case 2:
+      system("cls");
+      // importarCsvPaciente(listaPacientes);
+      break;
+    case 3:
+      system("cls");
+      // exportarCsvPaciente(listaPacientes);
+      break;
+    case 4:
+      system("cls");
+      printf("Saliendo...\n");
+      sleep(2);
+      system("cls");
+      break;
+    default:
+      printf("\nOpcion no valida. Intente nuevamente.\n\n");
+      system("pause");
+    }
+  } while (opcion != 10);
+
+  return 0;
+}
